492a9b095bcf0915926ffc79e098d8ef3706dbba
QuartzAl <55610038+QuartzAl@users.noreply.github.com>
2021-10-26 17:51:28 +0700

    Tugas 2 : base admin template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',  function () {
+    return redirect()->route('home');
 });
+
+Route::get('/home', ['as' => 'home', function () {
+    return view('content.home');
+}]);
